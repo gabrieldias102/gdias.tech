b594f82771b6a8dfbdaec6655d3fc9a366af2b4b
Gabriel Dias <contatogbd@gmail.com>
2024-08-16 10:14:11 -0300

    adjusting mobile layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 export default function Home() {
   return (
-    <div className="flex flex-col w-full h-screen text-center justify-center gap-8">
-      <h1 className="text-6xl font-bold uppercase">Under Construction</h1>
-      <p className="w-1/3 mx-auto text-lg">
+    <div className="flex flex-col w-full h-screen text-center justify-center gap-8 p-4">
+      <h1 className="text-4xl md:text-6xl font-bold uppercase">
+        Under Construction
+      </h1>
+      <p className="w-full md:w-1/2 lg:w-1/3 mx-auto text-base md:text-lg">
         🚧 Pardon the dust! I&apos;m busy coding like mad scientists 🧪🔧 to
         bring this site to life. Check back soon for something awesome! In the
         meantime, feel free to imagine a website so cool, it blows your socks
-        off 🧦💨
+        off 🧦💨!
       </p>
     </div>
   );
