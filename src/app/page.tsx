a7cba07a8867c3782d41a39d8e07b1098e41bd59
Gabriel Dias <contatogbd@gmail.com>
2024-08-16 09:43:33 -0300

    fixing vercel lint check

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,8 @@ export default function Home() {
     <div className="flex flex-col w-full h-screen text-center justify-center gap-8">
       <h1 className="text-6xl font-bold uppercase">Under Construction</h1>
       <p className="w-1/3 mx-auto text-lg">
-        🚧 Pardon the dust! I'm busy coding like mad scientists 🧪🔧 to bring
-        this site to life. Check back soon for something awesome! In the
+        🚧 Pardon the dust! I&apos;m busy coding like mad scientists 🧪🔧 to
+        bring this site to life. Check back soon for something awesome! In the
         meantime, feel free to imagine a website so cool, it blows your socks
         off 🧦💨!
       </p>
