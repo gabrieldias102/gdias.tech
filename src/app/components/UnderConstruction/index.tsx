496e146ad3f016bac48e3336df27d0efe3b746d6
Gabriel Dias <contatogbd@gmail.com>
2024-08-16 21:54:41 -0300

    fixing lint check and adding  again

--- src/app/components/UnderConstruction/index.tsx
+++ src/app/components/UnderConstruction/index.tsx
@@ -5,8 +5,8 @@ export default function UnderConstruction() {
         Under Construction
       </h1>
       <p className="w-full md:w-1/2 lg:w-1/3 mx-auto text-base md:text-lg">
-        🚧 Pardon the dust! I&apos;m busy coding like mad scientists 🧪🔧 to
-        bring this site to life. Check back soon for something awesome! In the
+        🚧 Pardon the dust! I'm busy coding like mad scientists 🧪🔧 to bring
+        this site to life. Check back soon for something awesome! In the
         meantime, feel free to imagine a website so cool, it blows your socks
         off 🧦💨!
       </p>
